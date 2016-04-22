cabffab41f83a0417c4623377b8c157e8978d906
windperson <windperson@gmail.com>
2016-04-22 18:06:47 +0800

    make distributable docker image script.

--- sh/devcli.sh
+++ sh/devcli.sh
@@ -2,6 +2,21 @@
 
 base_yml=docker-compose.python3.base.yml
 
+while getopts "hb" arg; do
+    case $arg in
+      h)
+        echo ""
+        echo "usage: [-h] [-b build but not run]"
+        echo ""
+        exit
+        ;;
+      b)
+        build_only=true
+        ;;
+    esac
+done
+
+
 if [ -z $PYTHON_VER ]
 then
     base_yml=docker-compose.python3.base.yml
@@ -10,7 +25,10 @@ else
 fi
 
 set -x
-docker-compose -f $base_yml build
+docker-compose -f ${base_yml} build
 docker-compose -f docker-compose.dev.yml build
-docker-compose -f docker-compose.dev.yml run --rm --name dev-cli cron-serv-dev-cli
+if [ -z $build_only ]
+then
+    docker-compose -f docker-compose.dev.yml run --rm --name dev-cli cron-serv-dev-cli
+fi
 exit
--- sh/remove_none_image.sh
+++ sh/remove_none_image.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
-docker rmi -f $(docker images -aq -f=dangling=true)
+
+while getopts "ha" arg; do
+    case $arg in
+      h)
+        echo ""
+        echo "usage: [-h] [-a clear_all]"
+        echo ""
+        exit
+        ;;
+      a)
+        clear_all=true
+        ;;
+    esac
+done
+
+if [ -z $clear_all ]
+then
+  set -e
+  docker rmi -f $(docker images -aq -f=dangling=true)
+  set +e
+else
+  set -e
+  docker rmi -f $(docker images -aq)
+  set +e
+fi
