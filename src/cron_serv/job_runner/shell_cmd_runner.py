8d5d8e746a1b5ce32f55579d11aa80dc8bcecbdb
windperson <windperson@gmail.com>
2016-04-29 03:59:13 +0800

    better error mesaage

--- src/cron_serv/job_runner/shell_cmd_runner.py
+++ src/cron_serv/job_runner/shell_cmd_runner.py
@@ -1,6 +1,7 @@
 import subprocess
 import sys
 import time
+import traceback
 from ..logger import ConsoleLogger
 
 
@@ -14,12 +15,15 @@ def gen_run_sh_job(command, logger=ConsoleLogger('cron_serv.local-runner')):
 
     def run_sh():
         logger.info("run scheduled job {0} at: {1}".format(command, time.strftime("%Y/%m/%d %H:%M:%S")))
+        # noinspection PyBroadException
         try:
             out = subprocess.check_output(args=[command], universal_newlines=True, shell=True)
             if out and len(out) > 0:
                 logger.info(out)
                 pass
+        except subprocess.CalledProcessError as e:
+            logger.error("Run sh job failed:\n%s", e.output)
         except:
-            logger.error("Run sh job failed:\n%s", sys.exc_info()[0])
+            logger.error("Run sh job failed with some other error!\n%s", traceback.format_exc())
 
     return run_sh
